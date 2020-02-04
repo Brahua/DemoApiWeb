81ddd530eccdace1f7c85047b2e86e3100259f00
Josue Bravo <josuebh62@gmail.com>
2020-02-04 18:59:15 -0500

    first commit

---
+++ DemoWebApi/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DemoWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApi.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Autor> Autor { get; set; }
+    }
+}
